20d504b4a249f7bafcafee95449d1fc60408a8ff
HN-MEDIA <83462108+HN-FILMCORNER@users.noreply.github.com>
2022-01-02 16:12:11 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,7 +8,7 @@ from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, OWNER_ID, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON
 from helper_func import subscribed, encode, decode, get_messages
-from database.sql import add_user, query_msg, full_userbase
+from database.mongo import add_user, query_msg, full_userbase
 
 
 #=====================================================================================##
